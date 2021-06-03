56efa42e4fddfd3e53b349e6c2da868bd55cf807
OkForg97 <okfrog97@yandex.ru>
2021-06-03 07:58:05 +0500

    CLUSTER.JS: add PID and CPUs count

--- cluster.js
+++ cluster.js
@@ -1,9 +1,11 @@
 const cluster = require('cluster');
 const os = require('os');
+const pid = process.pid
 
 if (cluster.isMaster) {
-    const cpusCount = os.cpus();
-    console.log(cpusCount);
+    const cpusCount = os.cpus().length;
+    console.log(`CPUs: ${cpusCount}`);
+    console.log(`Master started. PID: ${pid}`);
 }
 if (cluster.isWorker){
 
